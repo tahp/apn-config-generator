32352624a939dc7557d13576650f6ccc5c7f29bb
Tahp <djtahp@gmail.com>
2023-08-22 22:27:00 -0700

    WORKING APP

--- backend/server.js
+++ backend/server.js
@@ -1,62 +1,42 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
 
-// Define a route for generating the APN configuration profile
-app.get('/generate-profile', (req, res) => {
-  // Simulated user inputs (replace these with actual user inputs)
-  const apnName = 'MyAPN';
-  const apnUsername = 'username';
-  const apnPassword = 'password';
-  const mmscUrl = 'http://mmsc.example.com';
-  const mmsProxy = 'mms-proxy.example.com';
-  
-  // Generate the XML content for the configuration profile
-  const xmlContent = `
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>PayloadContent</key>
-      <array>
-        <dict>
-          <key>APN</key>
-          <string>${apnName}</string>
-          <key>Username</key>
-          <string>${apnUsername}</string>
-          <key>Password</key>
-          <string>${apnPassword}</string>
-          <key>MMSC</key>
-          <string>${mmscUrl}</string>
-          <key>MMSProxy</key>
-          <string>${mmsProxy}</string>
-          <!-- Add more APN settings here -->
-        </dict>
-      </array>
-      <key>PayloadDescription</key>
-      <string>Configures APN settings.</string>
-      <key>PayloadDisplayName</key>
-      <string>APN Configuration</string>
-      <key>PayloadIdentifier</key>
-      <string>com.example.apnconfig</string>
-      <key>PayloadType</key>
-      <string>Configuration</string>
-      <key>PayloadUUID</key>
-      <string>generated-uuid-here</string>
-      <key>PayloadVersion</key>
-      <integer>1</integer>
-    </dict>
-    </plist>
-  `;
-  
-  // Set response headers to indicate XML content
-  res.header('Content-Type', 'application/xml');
-  
-  // Send the generated XML content as the response
+const port = 3000; // Choose a port number
+
+// Use JSON middleware for parsing request bodies
+app.use(bodyParser.json());
+
+// Serve static files (if needed)
+app.use(express.static(path.join(__dirname, '../')));
+
+// Define an API endpoint for generating the profile
+app.post('/generate-profile', (req, res) => {
+  const formData = req.body;
+
+  // Generate XML configuration based on formData
+  const xmlContent = generateXml(formData);
+
+  // Respond with the XML content
+  res.type('application/xml');
   res.send(xmlContent);
 });
 
 // Start the server
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
+
+// Helper function to generate XML content
+function generateXml(formData) {
+  // Customize this function to generate the XML structure
+  const xml = `
+    <configuration>
+      <configName>${formData.configName}</configName>
+      <!-- Add other form fields here -->
+    </configuration>
+  `;
+  return xml;
+}
